ca6bbcaeb865dc664f49686721e13259c4eaa189
Minh Le <mtnatle@gmail.com>
2017-02-27 22:51:59 -0600

    WelcomePage,Login and Reistration screens

---
+++ Assets/Scripts/Registration.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Database : MonoBehaviour {
+    
+    public static string user = "", name= "";
+    private string password = "", repass= "";
+    private void OnGUI() {
+        
+    }
+}
