fc8e97e56708f08ef16699d976a3fb293621b8e2
EndianaJones <endypluviose@Gerve-Endys-MacBook-Pro.local>
2017-03-31 09:50:33 -0500

    Saving latest changes

---
+++ Assets/Scripts/ObjectTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTimer : MonoBehaviour {
+	//public float sec = 3f;
+	public GameObject gameObject;
+ 
+    void Start (){
+			//gameObject.SetActive(false);
+            StartCoroutine(RemoveAfterSeconds(2, gameObject));
+			
+    }
+    IEnumerator RemoveAfterSeconds (int seconds, GameObject obj){
+        yield return new WaitForSeconds(2);
+        obj.SetActive(false);
+    }
+
+	
+    
+}
