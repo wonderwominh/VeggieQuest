7d2654d8f4b799f808b4ec5a4161bfc7a187507f
EndianaJones <endypluviose@Gerve-Endys-MacBook-Pro.local>
2017-04-21 15:09:34 -0500

    Added TestingScene used in meeting on 4/21

---
+++ TestingScene/Assets/Scripts/Bounce.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	public float force = 500f;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Awake () {
+		anim = GetComponent<Animator>(); 
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D target)
+	{
+		if (target.gameObject.tag == "Player")
+		{
+			target.gameObject.GetComponent<playerMove>().BouncePlayer(force);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TestingScene/Assets/Scripts/TouchManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TouchManager :  MonoBehaviour {
+	
+	public static bool guiTouch = false;
+
+	public void TouchInput (GUITexture texture)
+	{
+		if(texture.HitTest(Input.GetTouch(0).position))
+		{
+			switch (Input.GetTouch(0).phase)
+			{
+			case TouchPhase.Began:
+				SendMessage("OnFirstTouchBegan");
+				SendMessage("OnFirstTouch");
+				guiTouch = true;
+				break;
+			case TouchPhase.Stationary:
+				SendMessage("OnFirstTouchStayed");
+				SendMessage("OnFirstTouch");
+				guiTouch = true;
+				break;
+			case TouchPhase.Moved:
+				SendMessage("OnFirstTouchMoved");
+				SendMessage("OnFirstTouch");
+				guiTouch = true;
+				break;
+			case TouchPhase.Ended:
+				SendMessage("OnFirstTouchEnded");
+				guiTouch = false;
+				break;
+			}
+		}
+		if(texture.HitTest(Input.GetTouch(1).position))
+		{
+			switch (Input.GetTouch(1).phase)
+			{
+			case TouchPhase.Began:
+				SendMessage("OnFirstTouchBegan");
+				SendMessage("OnFirstTouch");
+				break;
+			case TouchPhase.Stationary:
+				SendMessage("OnFirstTouchStayed");
+				SendMessage("OnFirstTouch");
+				break;
+			case TouchPhase.Moved:
+				SendMessage("OnFirstTouchMoved");
+				SendMessage("OnFirstTouch");
+				break;
+			case TouchPhase.Ended:
+				SendMessage("OnFirstTouchEnded");
+				break;
+			}
+		}
+	}
+
+}
