bd7ea923fc87a4c587ebca447b004f3a1d5cc239
Josue Caraballo <josue.caraballo@mavs.uta.edu>
2017-04-24 17:29:02 -0500

    Added Gerve's Test Scene

---
+++ Assets/Scripts/trap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trap : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "Player"){
+			Destroy(target.gameObject);
+			Destroy(gameObject);
+		}
+		
+	}
+	
+}
