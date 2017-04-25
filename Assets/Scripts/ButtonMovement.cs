2ed74b97aed54d2c4ad7121177d6f7570a74c747
EndianaJones <endypluviose@Gerve-Endys-MacBook-Pro.local>
2017-04-24 23:02:44 -0500

    apr 24 11:02 pm

---
+++ Assets/Scripts/ButtonMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ButtonMovement : TouchManager {
+	public enum type {LeftButton, RightButton, JumpButton};
+	public type buttonType;
+
+	public float jumpHeight = 0.0f, moveSpeed = 0.0f;
+
+	public GameObject playerObject = null;
+	Rigidbody2D playerRigidbody = null;
+	public GUITexture buttonTexture = null;
+
+	void Start () {
+		playerRigidbody= playerObject.GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		TouchInput (buttonTexture);
+	}
+
+	void OnFirstTouchBegan ()
+	{
+		switch (buttonType)
+		{
+			case type.JumpButton:
+				playerRigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+				break;
+		}
+	}
+
+	void OnSecondTouchBegan ()
+	{
+		switch (buttonType)
+		{
+			case type.JumpButton:
+				playerRigidbody.AddForce(Vector2.up * jumpHeight, ForceMode2D.Impulse);
+				break;
+		}
+	}
+
+	void OnFirstTouch ()
+	{
+		switch (buttonType)
+		{
+			case type.LeftButton:
+				playerObject.transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+				break;
+			case type.RightButton:
+				playerObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+				break;
+		}
+	}
+
+	void OnSecondTouch ()
+	{
+		switch (buttonType)
+		{
+			case type.LeftButton:
+				playerObject.transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+				break;
+			case type.RightButton:
+				playerObject.transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+				break;
+		}
+	}
+
+}
---
+++ Assets/Scripts/MenuCtrl2.0.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MenuCtrl : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+
+	public void OnPointerDown(PointerEventData data){
+		if (gameObject.name == "B_Login") {
+			SceneManager.LoadScene(Login);
+			
+		}	
+
+		if (gameObject.name == "B_Register") {
+			SceneManager.LoadScene(Registration);
+
+		}
+		
+	}	
+
+//	public void LoadScene(string sceneName) {
+//		SceneManager.LoadScene(sceneName);
+//
+//	}
+
+}
