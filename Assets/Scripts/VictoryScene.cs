30fa040108357271003a47ddcd79b96846b47d4e
Josue Caraballo <josue.caraballo@mavs.uta.edu>
2017-04-25 13:58:57 -0500

    Added Victory Scene and some Door logic to trigger that transtion.

---
+++ Assets/Scripts/VictoryScene.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScene : MonoBehaviour
+{
+    public Texture curtain;
+
+    private float alphaFadeValue;
+    private bool fadingOut;
+    private bool faded;
+
+    // Use this for initialization
+    void Start ()
+    {
+        alphaFadeValue = 0;
+        fadingOut = false;
+    }
+
+    private void OnGUI()
+    {
+        GUI.color = new Color(0, 0, 0, alphaFadeValue);
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), curtain);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (!faded)
+        {
+            if (fadingOut && alphaFadeValue < 1)
+            {
+                alphaFadeValue += Mathf.Clamp01(Time.deltaTime / 1);
+            }
+            else if (alphaFadeValue > 1)
+            {
+                alphaFadeValue = 1;
+            }
+            else if (alphaFadeValue == 1)
+            {
+                faded = true;
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene("VictoryScene", LoadSceneMode.Single);
+        }
+    }
+
+    public void onClick(string btnPushed)
+    {
+        if (btnPushed == "nextLevel")
+        {
+            //SceneManager.LoadSceneAsync("scene2", LoadSceneMode.Single);
+            Debug.Log("Go to the next level here!");
+        }
+        else if (btnPushed == "quit")
+        {
+            SceneManager.LoadSceneAsync("WelcomePage", LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.Log("Invalid button choice for victory scene.");
+        }
+    }
+
+    public void fadeOut()
+    {
+        fadingOut = true;
+    }
+
+    public bool isFaded()
+    {
+        return faded;
+    }
+}
